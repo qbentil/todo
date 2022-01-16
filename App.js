4706f0f109a53417653ded26d6288459fc6c2763
qbentil <Bentilshadrack72@gmail.com>
2022-01-16 01:45:03 +0000

    Adding Input UI

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
+import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native-web';
+import React, {useState} from 'react';
+import { StatusBar, setStatusBarBackgroundColor } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
 import Todos from './components/Todos'
 
 export default function App() {
+  const [task, setstate] = useState();
+
+  const addTodo = () => {
+    console.log(task);
+  }
+  
   return (
     <View style={styles.container}>
       <View style = {styles.wrapper}>
@@ -12,9 +20,20 @@ export default function App() {
 
       <View style = {styles.listItems}>
         <Todos />
-        <Todos />
-        <Todos />
       </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style = {styles.input} placeholder = "Write a task..." value = {task} onChangeText = {(text) => setTask(text)} />
+        <TouchableOpacity onClick = {this.addTodo}>
+          <View style = {styles.buttonWrapper}>
+            <Text style = {styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,5 +54,35 @@ const styles = StyleSheet.create({
   listItems: {
     marginTop: 20,
     paddingHorizontal: 5,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: '100%',
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input:{
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 2,
+  },
+  buttonWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 2,
+    
+  },
+  addText: {
+    
   },
 });
